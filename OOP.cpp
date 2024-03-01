0c16f0c6d7898f03ed44bfb8ef189bbf631687d3
Lorena Beatrice <Lorena@DESKTOP-KJNBG28>
2024-03-01 20:46:48 +0200

    problema 2 -> testata in code blocks

--- OOP.cpp
+++ OOP.cpp
@@ -1,29 +1,51 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
 using namespace std;
 
+char cuv[100][100];
+char text[100];
+int n;
+
+void Ordonare()
+{
+    bool ok=0;
+    while (!ok)
+    {
+        ok=1;
+        for(int j=0;j<n-1;j++)
+            if (strlen(cuv[j])<strlen(cuv[j+1]))
+            {
+                ok=0;
+                char aux[100];
+                strcpy(aux,cuv[j]);
+                strcpy(cuv[j],cuv[j+1]);
+                strcpy(cuv[j+1],aux);
+            }
+            else if (strlen(cuv[j])==strlen(cuv[j+1]))
+            {
+                if (strcmp(cuv[j],cuv[j+1])>0)
+                {
+                    ok=0;
+                    char aux[100];
+                    strcpy(aux,cuv[j]);
+                    strcpy(cuv[j],cuv[j+1]);
+                    strcpy(cuv[j+1],aux);
+                }
+            }
+    }
+}
 int main()
 {
-	///problema 1
-	int sum = 0;
-	FILE* fp;
-	char* line = new char[100];
-	size_t len;
-	fopen_s(&fp, "in.txt", "r");
-	if (fp == NULL)
-	{
-		printf("N-a mers");
-		exit(EXIT_FAILURE);
-	}
-	///functia fgets
-	while (fgets(line, 100, fp))
-	{
-		int nr = 0;
-		for (int i = 0; line[i] != NULL && line[i] != '\n'; i++) nr = nr * 10 + line[i] - '0';
-		sum += nr;
-	}
-	printf("%d", sum);
-
-	return 0;
+    ///problema 2
+    scanf("%[^\n]",text);
+    char* p=strtok(text," ");
+    while (p!=0)
+    {
+        strcpy(cuv[n++],p);
+        p=strtok(NULL," ");
+    }
+    Ordonare();
+    for(int j=0;j<n;j++) printf("%s\n",cuv[j]);
+    return 0;
 }
